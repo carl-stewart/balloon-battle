0b0dffa0b4a71a10e1d4c113bbeb4871fcf27cf1
Mariano Alipi <marianoalipi@gmail.com>
2019-03-13 23:30:30 -0600

    Added balloons and player flying sprites. Partially implemented flying animation.

--- src/dev/marianoalipi/balloonbattle/SpriteSheet.java
+++ src/dev/marianoalipi/balloonbattle/SpriteSheet.java
@@ -34,4 +34,12 @@ public class SpriteSheet {
     public BufferedImage crop (int x, int y, int width, int height) {
         return sheet.getSubimage(x, y, width, height);
     }
+    
+    public BufferedImage getSheet() {
+		return sheet;
+	}
+    
+    public void setSheet(BufferedImage sheet) {
+		this.sheet = sheet;
+	}
 }
