b7f10e460e5dc556a55e1f0f50ecb4d46c89efea
Mariano Alipi <marianoalipi@gmail.com>
2019-03-18 22:51:56 -0600

    Fixed static reference warning in MainMenu.java.

--- src/dev/marianoalipi/balloonbattle/menu/MainMenu.java
+++ src/dev/marianoalipi/balloonbattle/menu/MainMenu.java
@@ -76,7 +76,7 @@ public class MainMenu extends Menu {
         
         
         // Draw a balloon to indicate the cursor
-        g.drawImage(Assets.balloonsOne[0], 300, 320 + selected * 10 + selected * Assets.textScale * Assets.mainMenuOptions[selected].getHeight(), (int)(game.SCALE * 16), (int)(game.SCALE * 12), null);
+        g.drawImage(Assets.balloonsOne[0], 300, 320 + selected * 10 + selected * Assets.textScale * Assets.mainMenuOptions[selected].getHeight(), (int)(Game.SCALE * 16), (int)(Game.SCALE * 12), null);
         // Draw balloon's outline (debugging)
         //g.drawRect(300, 320 + selected * 10 + selected * Assets.textScale * Assets.mainMenuOptions[selected].getHeight(), 32, 32);
 	}
