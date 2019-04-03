1c894e2577579c64b3115d6665fb343e7cd9f711
Mariano Alipi <marianoalipi@gmail.com>
2019-04-03 15:48:06 -0600

    Created class Platform (bare-bones).

--- src/dev/marianoalipi/balloonbattle/levels/Level.java
+++ src/dev/marianoalipi/balloonbattle/levels/Level.java
@@ -1,20 +1,28 @@
 package dev.marianoalipi.balloonbattle.levels;
 
+import java.util.ArrayList;
+
 public class Level {
 
 	private byte id;
-	
+	private ArrayList<Platform> platforms;
 	
 	public Level() {
 		this.id = -1;
+		this.platforms = new ArrayList<Platform>();
 	}
 	
 	public Level(byte id) {
 		this.id = id;
+		this.platforms = new ArrayList<Platform>();
 	}
 	
 	public byte getId() {
 		return id;
+	}
+	
+	public ArrayList<Platform> getPlatforms() {
+		return platforms;
 	}
 	
 	public void setId(byte id) {
---
+++ src/dev/marianoalipi/balloonbattle/levels/Platform.java
@@ -0,0 +1,77 @@
+package dev.marianoalipi.balloonbattle.levels;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public class Platform {
+
+	private int x, y, width, height;
+	private Rectangle hitbox;
+	private BufferedImage sprite;
+	
+	public Platform() {
+		this.x = 0;
+		this.y = 0;
+		this.width = 0;
+		this.height = 0;
+		this.hitbox = new Rectangle(x, y, width, height);
+		this.sprite = null;
+	}
+	
+	public Platform(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.hitbox = new Rectangle(x, y, width, height);
+		this.sprite = null;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public Rectangle getHitbox() {
+		return hitbox;
+	}
+	
+	public BufferedImage getSprite() {
+		return sprite;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public void setHitbox(Rectangle hitbox) {
+		this.hitbox = hitbox;
+	}
+	
+	public void setSprite(BufferedImage sprite) {
+		this.sprite = sprite;
+	}
+}
